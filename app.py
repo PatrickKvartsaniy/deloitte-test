fe8b7b7ed6d96dd994a330a537909051e5e8ab7f
patrick <kvartsaniy@gmail.com>
2018-06-26 12:52:45 +0300

    Fixed host bug

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
@@ -58,4 +60,5 @@ def logout():
 
 
 if __name__ == "__main__":
-   app.run()
+    host, port = '0.0.0.0', int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
